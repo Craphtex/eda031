a2f452f103f66fc034d1be2ae80f0a01e0dbf085
Craphtex <christian@crapht.se>
2014-02-15 01:24:31 +0100

    Lab2, ExA4: Started working on solution

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -14,8 +14,8 @@ Dictionary::Dictionary() {
 }
 
 bool Dictionary::contains(const string& word) const {
-	for (string word_in_list : words) {
-		if (word_in_list == word) {
+	for (Word word_in_list : words) {
+		if (word_in_list.get_word() == word) {
 			return true;
 		}
 	}
@@ -32,13 +32,15 @@ void Dictionary::load_file(const string& in) {
 	if(input) {
 		string word;
 		while(input >> word) {
-			words.insert(word);
 			int number_of_trigrams = 0;
 			input >> number_of_trigrams;
+			vector<string> trigrams;
 			for (int i = 0; i < number_of_trigrams; ++i) {
 				string trigram;
 				input >> trigram;
+				trigrams.push_back(trigram);
 			}
+			words.insert(new Word(word, trigrams));
 		}
 	} else {
 		cout << "No wordlist to load.";
--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include "word.h"
 
 using namespace std;
 
@@ -14,7 +15,9 @@ public:
 	vector<string> get_suggestions(const string& word) const;
 	void load_file(const string& input);
 private:
-	unordered_set<string> words;
+	unordered_set<Word> words;
+	vector<Word> words[25]; // words[i] = the words with i letters,
+	                        // ignore words longer than 25 letters
 };
 
 #endif
