57aab108cd5eccc63d3340b3c704335d60c70471
Craphtex <christian@crapht.se>
2014-02-15 01:11:49 +0100

    Lab2, ExA3, added

--- lab2/word.cc
+++ lab2/word.cc
@@ -4,12 +4,28 @@
 
 using namespace std;
 
-Word::Word(const string& w, const vector<string>& t) {}
+Word::Word(const string& w, const vector<string>& t): word(w), triples(t) {}
 
 string Word::get_word() const {
-	return string();
+	return word;
 }
 
 unsigned int Word::get_matches(const vector<string>& t) const {
-	return 0;
+	int triplets_ctr = 0;
+	int t_ctr = 0;
+	int matches_ctr = 0;
+	while(triplets.size() < triplets_ctr || t.size() < t_ctr) {
+		if (triplets[triplets_ctr] == t[t_ctr]) {
+			++matches_ctr;
+			++triplets_ctr;
+			++t_ctr;
+		}
+		else if(triplets[triplets_ctr] < t[t_ctr]) {
+			++triplets_ctr;
+		}
+		else {
+			++t_ctr;
+		}
+	}
+	return matches_ctr;
 }
