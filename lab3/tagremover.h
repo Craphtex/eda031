775dc46241b3b6ef4d754666214d8076fe53fba4
Craphtex <christian@crapht.se>
2014-02-24 23:12:10 +0100

    Lab3,ExA1: completed

--- lab3/tagremover.h
+++ lab3/tagremover.h
@@ -1,16 +1,19 @@
 #ifndef TAGREMOVER_H
 #define TAGREMOVER_H
 
+#include <string>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class TagRemover {
 public:
 	TagRemover(istream& in);
-	void print(const ostream& out) const;
+	void print(ostream& out) const;
 private:
 	istream& in;
+	unordered_map <string, char> specialCharacter;
 };
 
 #endif
