0130c86eb4ce9dc8edf98acf3a9fff0f20efa780
Craphtex <christian@crapht.se>
2014-03-06 12:33:33 +0100

    Lab4,Ex4: Complete

--- lab4/bitset/bitreference.cc
+++ lab4/bitset/bitreference.cc
@@ -2,13 +2,23 @@
 #include "bitset.h"
 
 BitReference& BitReference::operator=(bool b) {
+	if (b) {
+		p_bits |= 1L << pos;
+	} else {
+		p_bits &= ~ (1L << pos);
+	}
 	return *this;
 }
 
 BitReference& BitReference::operator=(const BitReference& rhs) {
+	if (rhs) {
+		p_bits |= 1L << pos;
+	} else {
+		p_bits &= ~ (1L << pos);
+	}
 	return *this;
 }
 
 BitReference::operator bool() const {
-	return true;
+	return (p_bits & (1L << pos)) != 0;
 }
